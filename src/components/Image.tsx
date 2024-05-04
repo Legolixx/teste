e4b98c53e43344201043d4025061d3d385f877d6
VICTOR <95864890+Legolixx@users.noreply.github.com>
2024-05-04 11:59:39 -0300

    Update Image.tsx

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,25 +1,40 @@
-import Image from "next/image";
 import { JSONData } from "@xata.io/client";
 import { ImgRecord } from "@/utils/xata";
 
 interface Props {
   images: JSONData<ImgRecord>[];
 }
+
 export default function VideoComp({ images }: Props) {
-    return (
-      <div>
-        {images.map((image) => (
-          <video key={image.id} width={500} height={500} controls preload="none" muted autoPlay loop className=" rounded-lg">
-            <source src={image.img?.url ?? ''} type="video/mp4" />
-            <track
-              src={image.img?.url ?? ''}
-              kind="subtitles"
-              srcLang="en"
-              label="English"
-            />
-            Your browser does not support the video tag.
-          </video>
-        ))}
-      </div>
-    );
-  }
+  const playVideo = (event) => {
+    event.target.play();
+  };
+
+  return (
+    <div>
+      {images.map((image) => (
+        <video
+          key={image.id}
+          width={1000}
+          height={1000}
+          controls
+          preload="none"
+          autoPlay
+          muted
+          loop
+          playsInline // Add playsInline attribute
+          onClick={playVideo}
+        >
+          <source src={image.img?.url ?? ''} type="video/mp4" />
+          <track
+            src={image.img?.url ?? ''}
+            kind="subtitles"
+            srcLang="en"
+            label="English"
+          />
+          Your browser does not support the video tag.
+        </video>
+      ))}
+    </div>
+  );
+}
